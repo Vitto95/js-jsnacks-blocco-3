bd154cbe62f181c3ce57ee6581ab5073c70907d3
Vitto95 <grimaldi.vitto@gmail.com>
2021-04-27 19:18:53 +0200

    snack 5 impostato, ma non completamente corretto

---
+++ assets/js/jsnack5.js
@@ -0,0 +1,41 @@
+var characters = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
+var charactersLength = characters.length;
+console.log(charactersLength);
+var basketballTeam = [
+  {
+    "Nome Giocatore": "giocatore1",
+  },
+  {
+    "Nome Giocatore": "giocatore2",
+  },
+  {
+    "Nome Giocatore": "giocatore3",
+  },
+  {
+    "Nome Giocatore": "giocatore4",
+  },
+  {
+    "Nome Giocatore": "giocatore5",
+  },
+];
+var limit = 3;
+
+//genero codice giocatore casuale
+function getRandomCode(chars, length) {
+  var playerCode = "";
+  for (var i = 0; i < limit; i++) {
+    playerCode += chars.charAt(Math.floor(Math.random() * length + 1));
+  }
+  for (var j = 0; j < limit; j++) {
+    playerCode += Math.floor(Math.random() * 9) + 1;
+  }
+  return playerCode;
+}
+
+for (var player of basketballTeam) {
+  player.code = getRandomCode(characters, charactersLength);
+  player.avgPt = Math.floor(Math.random() * 51);
+  player.threePointsPerc = Math.floor(Math.random() * 101) + "%";
+}
+
+console.log(basketballTeam);
